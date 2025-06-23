9942e8b5c57da2dd1c91fc9a815e9318d7adf3dc
calltj <Timileyinadewara2@gmail.com>
2025-06-23 11:34:01 +0100

    Refactor index.js to remove unused route handlers

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,6 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.sendFile(require('path').join(__dirname, '../public/index.html'));
-});
-router.get('/download', function(req, res, next) {
-  res.sendFile(require('path').join(__dirname, '../public/download.html'));
-});
-router.get('/business-plan', function(req, res, next) {
-  res.sendFile(require('path').join(__dirname, '../public/business-plan.html'));
-});
 
 module.exports = router;
